6fbbd8f0c2c59cbf54cb18e1ebf4c06dc368c32b
CherishShed <c.shed2000@gmail.com>
2024-02-14 11:05:39 +0100

    initial commit

---
+++ src/Middlewares/expressMiddeware.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const corsOptions = {
+  origin: '*',
+}
+
+app.use(cors(corsOptions))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+export default app
