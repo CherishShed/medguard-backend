d672bbf3c98a5c265d5e76e0461bc789688920ad
CherishShed <c.shed2000@gmail.com>
2024-03-13 12:59:28 +0100

    removed vonage api middleware

--- src/Middlewares/smsMiddleware.ts
+++
@@ -1,9 +0,0 @@
-import { Vonage } from '@vonage/server-sdk'
-import { AuthInterface } from '@vonage/auth'
-import 'dotenv/config'
-const vonage = new Vonage({
-  apiKey: process.env.VONAGE_API_KEY,
-  apiSecret: process.env.VONAGE_API_SECRET,
-} as AuthInterface)
-
-export default vonage
